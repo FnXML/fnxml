fa7e4405ceb69a74d41fe639855ee70ecc857f26
Doug ClymerOlson <doug@yarina.org>
2024-10-01 20:14:02 -0600

    update stream_decoder to better decode a stream of elements.  It is now possible to use a handler to convert elements to other forms in the decode process.

--- lib/stream_decoder_default.ex
+++ lib/stream_decoder_default.ex
@@ -21,17 +21,29 @@ defmodule FnXML.Stream.Decoder.Default do
   case 1: only one element on the stack, reverse the element and return it
   case 2: more than one element on the stack, reverse the top element and add it to the second element
   """
-  def handle_close(_meta, _path, [h], _opts), do: {h |> Enum.reverse(), []}
+  def handle_close(_meta, path, [h], opts) do
+    element = process_element(h |> Enum.reverse(), path, opts)
+    {element, []}
+  end
 
-  def handle_close(_meta, _path, [h, p | anc], _opts),
-    do: [[{:child, h |> Enum.reverse()} | p] | anc]
+  def handle_close(_meta, path, [h, p | anc], opts) do
+    element = process_element(h |> Enum.reverse(), path, opts)
+    [[element | p] | anc]
+  end
+
+  def process_element(element, [_], []), do: element
+  def process_element(element, _path, []), do: {:child, element}
+  def process_element(element, path, opts) do
+    handler = Keyword.get(opts, :handle_element, fn element, _path, _opts -> {:child, element} end)
+    handler.(element, path, opts)
+  end
+  
+  @impl true
+  def handle_prolog(meta, _path, acc, _opts), do: [{:prolog, meta} |acc]
 
   @impl true
-  def handle_prolog(_meta, _path, acc, _opts), do: acc
+  def handle_comment(meta, _path, acc, _opts), do: [{:comment, meta} | acc]
 
   @impl true
-  def handle_comment(_meta, _path, acc, _opts), do: acc
-
-  @impl true
-  def handle_proc_inst(_meta, _path, acc, _opts), do: acc
+  def handle_proc_inst(meta, _path, acc, _opts), do: [{:proc_inst, meta} | acc]
 end
--- test/stream_decoder_test.exs
+++ test/stream_decoder_test.exs
@@ -2,10 +2,12 @@ defmodule FnXML.Stream.DecoderTest do
   use ExUnit.Case
 
   alias FnXML.Stream.Decoder
+  alias FnXML.Element
 
   doctest Decoder
   doctest Decoder.Default
 
+  @tag focus: true
   test "tag" do
     result =
       [open: [tag: "a"], close: [tag: "a"]]
@@ -49,5 +51,55 @@ defmodule FnXML.Stream.DecoderTest do
              child: [tag: "child", attributes: [{"b", "2"}], text: [content: "child world"]],
              text: [content: "world"]
            ]
+  end
+
+
+  def handler(element, path, opts, acc \\ %{})
+  def handler([], _path, _opts, element), do: {element.tag |> elem(0) |> String.to_atom(), element}
+  def handler([item | rest], path, opts, acc), do: handler(rest, path, opts, handle_item(item, acc))
+
+  def handle_item({:tag, _} = tag, acc), do: Map.merge(acc, %{ tag: Element.tag([tag]), content: []})
+  def handle_item({:attributes, attrs}, acc), do: Map.merge(acc, %{ attributes: attrs})
+  def handle_item({:loc, _}, acc), do: acc
+  def handle_item({:text, [{:content, t}]}, acc), do: %{acc | content: Listy.insert(acc.content, {:text, t})}
+  def handle_item(item, acc), do: %{acc | content: Listy.insert(acc.content, item)}
+
+
+  test "decode using handler" do
+                                        
+    result =
+      [
+        open: [tag: "ns:hello", attributes: [{"a", "1"}]],
+        text: [content: "hello"],
+        open: [tag: "child", attributes: [{"b", "2"}]],
+        text: [content: "child world"],
+        open: [tag: "grandchild", attributes: [{"c", "3"}]],
+        close: [tag: "grandchild"],
+        close: [tag: "child"],
+        text: [content: "world"],
+        close: [tag: "ns:hello"]
+      ]
+      |> FnXML.Stream.Decoder.decode(FnXML.Stream.Decoder.Default, handle_element: &handler/3)
+      |> Enum.at(0)
+
+    assert result == {
+      :hello,
+      %{
+        attributes: [{"a", "1"}],
+        tag: {"hello", "ns"},
+        content: [
+          text: "world",
+          child: %{
+            attributes: [{"b", "2"}],
+            tag: {"child", ""},
+            content: [
+              grandchild: %{attributes: [{"c", "3"}], tag: {"grandchild", ""}, content: []},
+              text: "child world"
+            ]
+          },
+          text: "hello"
+        ]
+      }
+    }
   end
 end
